d85cd8dba1d6c2d3b73ffb226dd129aaf2326f77
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-06-29 20:29:27 +0530

    fix attempt #1

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"*": {"origins": "*"}})
 
 
 @app.route("/", methods=["GET"])
 def index():
-    return jsonify({"message": "Hello World"})
+    return jsonify({"message": "Hello friend."})
 
 
 if __name__ == "__main__":
